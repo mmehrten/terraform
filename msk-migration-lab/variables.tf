543167ce3827f16fe6c424693f8cfe4b26035485
M Mehrtens <m@mehrten.dev>
2024-02-22 10:43:57 -0600

    Add samples for MSK migration lab

---
+++ msk-migration-lab/variables.tf
@@ -0,0 +1,31 @@
+variable "account-id" {
+  type        = string
+  description = "The account to create resources in."
+}
+variable "app-shorthand-name" {
+  type        = string
+  description = "The shorthand name of the app being provisioned."
+}
+variable "app-name" {
+  type        = string
+  description = "The longhand name of the app being provisioned."
+}
+variable "cidr-block" {
+  type        = string
+  description = "The root CIDR block for the VPC"
+}
+variable "public-subnets" {
+  type        = map(string)
+  description = "A mapping of Availability Zone to the CIDR block for the subnet in that AZ."
+}
+variable "private-subnets" {
+  type        = map(string)
+  description = "A mapping of Availability Zone to the CIDR block for the subnet in that AZ."
+}
+variable "private-cert-passphrase" {
+  description = "The passphrase for the client certificate for MSK"
+  type        = string
+}
+variable "vpc-id" {
+  type = string
+}
